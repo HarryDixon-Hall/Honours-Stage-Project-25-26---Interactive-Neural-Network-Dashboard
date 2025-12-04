f3e546c06af6394890a638575d5b993b4103d5b0
HarryDixon-Hall <H.DIXON-HALL-2021@hull.ac.uk>
2025-12-04 16:02:09 +0000

    Completed the implementation of methods for the nn_model and trainer - the nn_model is a basic framework for future neural networks which are more complicated such as convolutional or recurrent. The trianer file must pull in those methods to display the training problems in the UI

--- trainer.py
+++ trainer.py
@@ -1,1 +1,18 @@
 from nn_model import SimpleNN
+import numpy as np
+
+def train_model(X_train, y_train, epochs=50, learning_rate=0.01, hidden_size=8):
+    """Train and return model + history"""
+    model = SimpleNN(input_size=X_train.shape[1],
+                     hidden_size=hidden_size,
+                     output_size=len(np.unique(y_train)))
+   
+    history = {'loss': [], 'accuracy': []}
+   
+    for epoch in range(epochs):
+        loss, acc = model.train_epoch(X_train, y_train, learning_rate)
+        history['loss'].append(loss)
+        history['accuracy'].append(acc)
+   
+    return model, history
+ 
