8cddc861c90ec371a14291abad097c6c95663605
HarryDixon-Hall <H.DIXON-HALL-2021@hull.ac.uk>
2026-03-02 09:29:09 +0000

    imported methods into test_dataload.py

---
+++ tests/test_dataload.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pytest
+
+from dataload import (
+    load_dataset_iris,
+    load_dataset_digits,
+    load_dataset_wine,
+    load_dataset,
+    get_dataset_stats,
+)
+
